aa17feb1e29cc56783eb5b88e6e59d2089ff7d2c
Jozef Bonnar <42479938+Alienwarez123@users.noreply.github.com>
2021-06-03 17:25:34 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+postcodefirst = "SE1 7P"
+postcodelastdigit = "B"
+houseaddress = "The Queen Buckingham Palace London"
+housecity = "London"
+emailaddress = "X"
+curryspassword = "X"
+cardnumber = "000000000000000"
+cardholder = "J Alienwarez"
+cardmonth = "00"
+cardyear = "00"
+cardcvv = "000"
+sitetimeout = 20
+discordmention = "<@DISCORD_ID>"
+discordwebhook = "https://discord.com/api/webhooks/X/X"
+sitelink2 = [
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/gigabyte-geforce-rtx-3070-8-gb-gaming-oc-graphics-card-10216240-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/msi-geforce-rtx-3070-8-gb-suprim-graphics-card-10220317-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/msi-geforce-rtx-3070-8-gb-gaming-x-trio-graphics-card-10217077-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/office-supplies/paper/canon-a4-premium-black-label-paper-500-sheets-10114701-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/asus-geforce-rtx-3070-8-gb-dual-graphics-card-10215777-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/asus-geforce-rtx-3070-8-gb-tuf-oc-gaming-graphics-card-10217338-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/gigabyte-geforce-rtx-3070-8-gb-vision-oc-graphics-card-10218742-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/gigabyte-geforce-rtx-3070-8-gb-eagle-graphics-card-10217336-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/msi-geforce-rtx-3070-8-gb-ventus-3x-oc-graphics-card-10217078-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/gigabyte-geforce-rtx-3070-8-gb-eagle-oc-graphics-card-10216239-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/gigabyte-geforce-rtx-3070-8-gb-aorus-master-graphics-card-10218741-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/asus-geforce-rtx-3070-8-gb-rog-strix-gaming-oc-graphics-card-10215770-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/msi-geforce-rtx-3070-8-gb-suprim-x-graphics-card-10219343-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/asus-geforce-rtx-3070-8-gb-dual-oc-graphics-card-10215776-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/asus-geforce-rtx-3070-10-gb-rog-strix-oc-graphics-card-white-10220919-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/msi-geforce-rtx-3070-8-gb-ventus-2x-oc-graphics-card-10217083-pdt.html",
+        "https://www.currys.co.uk/gbuk/computing-accessories/components-upgrades/graphics-cards/pny-geforce-rtx-3070-8-gb-xlr8-gaming-revel-edition-graphics-card-10218690-pdt.html",
+
+    ]
